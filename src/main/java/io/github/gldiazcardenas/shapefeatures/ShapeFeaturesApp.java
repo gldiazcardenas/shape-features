8aee0818f8b0597b40e68d2596fc009e0d7800d3
Gabriel Diaz <gabriel.leonardo.diaz@gmail.com>
2023-11-04 07:04:50 -0500

    Initial project structure

---
+++ src/main/java/io/github/gldiazcardenas/shapefeatures/ShapeFeaturesApp.java
@@ -0,0 +1,9 @@
+package io.github.gldiazcardenas.shapefeatures;
+
+public class ShapeFeaturesApp {
+
+    public static void main(String[] arg) {
+        System.out.println("Hello World!");
+    }
+
+}
